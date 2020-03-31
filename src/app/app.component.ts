6f866d6796ec1a0d38bdcb85fda0fa4218b193cf
Brian Otieno <brianokola@gmail.com>
2020-03-31 15:38:35 +0300

    Generated a component for the quotes and interpolated them to the root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,8 @@ export class AppComponent {
   title = 'Quotes App';
 
   quotes: Quote[] = [
-    { id: 1, name: 'Coders are fueled by coffee' },
-    { id: 2, name: 'Lms is not a Bible' },
-    { id: 3, name: 'Prepare to master the internet' }
+    { id: 1, name: 'Coders are fueled by coffee', author: 'David' },
+    { id: 2, name: 'Lms is not a Bible', author: 'David Kahara' },
+    { id: 3, name: 'Prepare to master the internet', author: 'Gynn Glen' }
   ];
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+  quotes: Quote[] = [
+    { id: 1, name: 'Coders are fueled by coffee', author: 'David Baldaci' },
+    { id: 2, name: 'Lms is not a Bible', author: 'David Kahara' },
+    { id: 3, name: 'Prepare to master the internet', author: 'Gynn Glen' }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit() {}
 }
